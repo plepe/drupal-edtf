3a2a6201b118b550c35e23a936e7f9a1be71f69f
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-12-20 21:00:46 +0100

    Validate date input (level 0)

--- src/Plugin/Field/FieldWidget/EDTFDefaultFieldWidget.php
+++ src/Plugin/Field/FieldWidget/EDTFDefaultFieldWidget.php
@@ -6,6 +6,7 @@ use Drupal\Core\Datetime\DateHelper;
 use Drupal\Core\Field\WidgetBase;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
  * Plugin implementation of the 'edtf_widget' widget.
@@ -31,10 +32,32 @@ class EDTFDefaultFieldWidget extends WidgetBase {
     $element['value'] = $element + [
       '#type' => 'textfield',
       '#default_value' => $default_value,
+      '#element_validate' => [[static::class, 'validateElement']],
       '#required' => $element['#required'],
       '#attributes' => ['class' => ['edtf']],
     ];
 
     return $element;
+  }
+
+  /**
+   * Form element validation handler for an 'edtf' element.
+   *
+   * Disallows saving invalid EDTF values.
+   */
+  public static function validateElement($element, FormStateInterface $form_state, $form) {
+    $reg_year = "[0-9]{4}";
+    $reg_month = "-(0[1-9]|1[012])";
+    $reg_day = "-(0[1-9]|[12][0-9]|3[01])";
+    $reg_time = "T([01][0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9]|:60)?";
+    $reg_shift = "(|Z|[-+][0-9]{2}(:[0-5][0-9])?)";
+
+    $reg_datetime = "{$reg_year}({$reg_month}({$reg_day}({$reg_time}($reg_shift)?)?)?)?";
+    $reg_date = "{$reg_year}({$reg_month}({$reg_day})?)?";
+
+    if (!preg_match("/^({$reg_datetime}|{$reg_date}\/{$reg_date})$/", $element['#value'])) {
+      $form_state->setError($element, new TranslatableMarkup('Invalid EDTF date entered.'));
+      return;
+    }
   }
 }
