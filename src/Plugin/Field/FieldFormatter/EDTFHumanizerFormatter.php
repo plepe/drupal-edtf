e7a8ab76ff8e855652d9f120172505ec9182de12
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-12-21 19:54:23 +0100

    Use ProfessionalWiki/EDTF for formatting

---
+++ src/Plugin/Field/FieldFormatter/EDTFHumanizerFormatter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\edtf\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Datetime\DateHelper;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Datetime\DrupalDateTime;
+use EDTF\EdtfFactory;
+
+/**
+ * Plugin implementation of the 'Humanizer' formatter for 'edtf' fields.
+ *
+ * @FieldFormatter(
+ *   id = "edtf_humanizer",
+ *   label = @Translation("EDTF Humanizer"),
+ *   field_types = {
+ *     "edtf"
+ *   }
+ * )
+ */
+class EDTFHumanizerFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+
+    $parser = \EDTF\EdtfFactory::newParser();
+    $humanizer = \EDTF\EdtfFactory::newHumanizerForLanguage('en');
+    $element = [];
+
+    foreach ($items as $delta => $item) {
+      $value = $parser->parse($item->value);
+      $edtfValue = $value->getEdtfValue();
+
+      $element[$delta] = ['#markup' => $humanizer->humanize($edtfValue)];
+    }
+
+    return $element;
+  }
+
+}
