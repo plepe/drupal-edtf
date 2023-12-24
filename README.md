dc776a55fad407c2af79b7d4e14f21d7ca9485c1
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2023-12-24 11:36:25 +0100

    Add Twig filters

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ When displaying timestamps, two formatters are available:
 This module adds field tokens:
 * `[node:field_name:year]` - show only the specified year
 * `[node:field_name:humanized]` - the timestamp in humanized form
+
+This module adds a few Twig filters for processing EDTF values:
+* `{{ value|edtf_validate }}` - check whether the value is valid (returns boolean).
+* `{{ value|edtf_humanize }}` - humanize the value in the current language (returns string or null if the value was not valid).
+* `{{ value|edtf_year }}` - returns the (minimum) year (returns int or null if the value was not valid).
+* `{{ value|edtf_min }}` - returns the earliest UNIX timestamp (returns int or null if the value was not valid).
+* `{{ value|edtf_max }}` - returns the latest UNIX timestamp (returns int or null if the value was not valid).
---
+++ src/TwigEDTFExtension.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Drupal\edtf;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use EDTF\EdtfFactory;
+
+/**
+ * Twig extension for working with Extended Date/Time Format (EDTF) values.
+ */
+class TwigEDTFExtension extends AbstractExtension {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFilters(): array {
+
+    $filters = [
+      new TwigFilter('edtf_validate', [$this, 'edtfValidate']),
+      new TwigFilter('edtf_humanize', [$this, 'edtfHumanize']),
+      new TwigFilter('edtf_year', [$this, 'edtfYear']),
+      new TwigFilter('edtf_min', [$this, 'edtfMin']),
+      new TwigFilter('edtf_max', [$this, 'edtfMax']),
+    ];
+    
+    return $filters;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function init(): void {
+    if (!isset($this->parser)) {
+      $this->parser = \EDTF\EdtfFactory::newParser();
+      $langcode = \Drupal::languageManager()->getCurrentLanguage()->getId();
+      $this->humanizer = \EDTF\EdtfFactory::newHumanizerForLanguage($langcode);
+    }
+  }
+
+  /**
+   * Checks the validty of an EDTF value.
+   */
+  public function edtfValidate (string $value): bool {
+    $this->init();
+
+    $parsingResult = $this->parser->parse($value);
+    return $parsingResult->isValid();
+  }
+
+
+  /**
+   * Returns an humanized version of an EDTF value.
+   */
+  public function edtfHumanize (string $value): string|null {
+    $this->init();
+
+    $parsingResult = $this->parser->parse($value);
+    if (!$parsingResult->isValid()) {
+      return null;
+    }
+
+    $edtfValue = $parsingResult->getEdtfValue();
+
+    return $this->humanizer->humanize($edtfValue);
+  }
+
+  /**
+   * Returns the year of an EDTF value.
+   */
+  public function edtfYear (string $value): int|null {
+    $this->init();
+
+    $parsingResult = $this->parser->parse($value);
+    if (!$parsingResult->isValid()) {
+      return null;
+    }
+
+    $edtfValue = $parsingResult->getEdtfValue();
+
+    return $edtfValue->getYear();
+  }
+
+  /**
+   * Returns the earliest timestamp of an EDTF value.
+   */
+  public function edtfMin (string $value): int|null {
+    $this->init();
+
+    $parsingResult = $this->parser->parse($value);
+    if (!$parsingResult->isValid()) {
+      return null;
+    }
+
+    $edtfValue = $parsingResult->getEdtfValue();
+
+    return $edtfValue->getMin();
+  }
+
+  /**
+   * Returns the latest timestamp of an EDTF value.
+   */
+  public function edtfMax (string $value): int|null {
+    $this->init();
+
+    $parsingResult = $this->parser->parse($value);
+    if (!$parsingResult->isValid()) {
+      return null;
+    }
+
+    $edtfValue = $parsingResult->getEdtfValue();
+
+    return $edtfValue->getMax();
+  }
+}
